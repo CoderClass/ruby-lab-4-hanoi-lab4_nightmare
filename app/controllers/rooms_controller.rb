f522d96cd95b00dddf9e5fd0a05f76a2cb23db51
Thanh Khac <kingkid162@gmail.com>
2016-11-01 19:30:26 +0700

    milestone 1 complete

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,27 @@
+class RoomsController < ApplicationController
+
+	def index
+		@room = Room.find params[:id]
+	end
+
+	def new
+		@room = Room.new
+	end
+
+	def create
+		@room = Room.create room_params
+		if @room.save
+			flash[:success] = "Create Room success."
+			redirect_to root_path
+		else
+			flash[:error] = "Error: #{@room.errors.full_messages.to_sentence}"
+			render :new
+		end
+	end
+
+	private
+
+	def room_params
+		params.require(:room).permit(:name)
+	end
+end
