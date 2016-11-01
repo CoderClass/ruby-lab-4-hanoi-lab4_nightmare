e335fc6ca433d0aca383e56adbb05e29967972b7
jundalisay <jundalisay@yahoo.com>
2016-11-01 20:17:30 +0700

    pushing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+
+  def logout
+    session[:username] = nil
+    redirect_back fallback_location: root_path
+  end
+
+  def login
+    session[:username] = params[:username].presence
+    redirect_back fallback_location: root_path
+  end
 end
